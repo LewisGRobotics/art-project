44ecbc5b4ceb97f5e97f9fe80458e96d2791c8af
Luis García Montañés <gamo.luis@gmail.com>
2021-05-13 10:19:33 +0200

    Add randomized shapes

--- art-project/src/app/polygon/polygon.component.ts
+++ art-project/src/app/polygon/polygon.component.ts
@@ -9,6 +9,8 @@ export class PolygonComponent implements OnInit {
 
   colorProperty = "#8f8f8f";
   colorVector = ["#272822","#c03", "#429032", "#2963bd","#c90"];
+  shapeVector = ["square", "bottomLeft", "topRight", "circle"];
+  classSelector = "bottomLeft";
   constructor() {
     
    }
@@ -16,6 +18,8 @@ export class PolygonComponent implements OnInit {
   ngOnInit(): void {
     const colorIndex = Math.floor(Math.random() * 4) + 1;
     this.colorProperty= this.colorVector[colorIndex];
+    const shapeIndex = Math.floor(Math.random() * 4);
+    this.classSelector = this.shapeVector[shapeIndex];
   }
 
 }
