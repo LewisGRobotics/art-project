1d846e9e4018a1170621b4687981a5a58d18022d
Luis García Montañés <gamo.luis@gmail.com>
2021-05-13 11:15:20 +0200

    Add more triangle and blank shapes

--- art-project/src/app/polygon/polygon.component.ts
+++ art-project/src/app/polygon/polygon.component.ts
@@ -9,7 +9,7 @@ export class PolygonComponent implements OnInit {
 
   colorProperty = "#8f8f8f";
   colorVector = ["#272822","#c03", "#429032", "#2963bd","#c90"];
-  shapeVector = ["square", "bottomLeft", "topRight", "circle"];
+  shapeVector = ["blank", "square", "bottomLeft", "bottomRight", "topLeft", "topRight", "circle"];
   classSelector = "bottomLeft";
   constructor() {
     
@@ -18,7 +18,7 @@ export class PolygonComponent implements OnInit {
   ngOnInit(): void {
     const colorIndex = Math.floor(Math.random() * 4) + 1;
     this.colorProperty= this.colorVector[colorIndex];
-    const shapeIndex = Math.floor(Math.random() * 4);
+    const shapeIndex = Math.floor(Math.random() * 7);
     this.classSelector = this.shapeVector[shapeIndex];
   }
 
