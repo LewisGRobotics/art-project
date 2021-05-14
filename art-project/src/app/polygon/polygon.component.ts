5e5e48c75e1cb67399ce1e13f22b971646bc97ec
Luis García Montañés <gamo.luis@gmail.com>
2021-05-14 14:09:09 +0200

    Random polygon generation on click

--- art-project/src/app/polygon/polygon.component.ts
+++ art-project/src/app/polygon/polygon.component.ts
@@ -6,22 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./polygon.component.scss']
 })
 export class PolygonComponent implements OnInit {
-
-  colorProperty = "#8f8f8f";
+  
   colorVector = ["#272822","#c03", "#429032", "#2963bd","#c90"];
   shapeVector = ["blank", "square", "bottomLeft", "bottomRight", "topLeft", "topRight", "circle"];
-  classSelector = "bottomLeft";
-  constructor() {
-    
-   }
+  color = "#8f8f8f";
+  colorSelector = "#8f8f8f";
+  shape = "bottomLeft";
+  shapeSelector = "bottomLeft";
+  toggled = false;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  polygonClicked(): void {    
+    this.generateRandom();
+
+  }
+
+  ngOnInit(): void {    
+    this.generateRandom();
+  }
+
+  // Generate a random polygon
+  generateRandom(): void {
     const colorIndex = Math.floor(Math.random() * 4) + 1;
-    this.colorProperty= this.colorVector[colorIndex];
+    this.colorSelector= this.colorVector[colorIndex];
+    this.color = this.colorSelector;
+    
     const shapeIndex = Math.floor(Math.random() * 7);
-    this.classSelector = this.shapeVector[shapeIndex];
+    this.shapeSelector = this.shapeVector[shapeIndex];
+    this.shape = this.shapeSelector;
 
-    if(this.classSelector == "blank") this.colorProperty = "#fff";
+    if(this.shapeSelector == "blank") this.colorSelector = "#fff";
+    if(this.shapeSelector == "bottomLeft") this.colorSelector = "7.5vh solid $g";
+  }
+
+  // Hide/show polygon
+  hide(): void{    
+    //this.colorSelector = this.toggled ? this.color : "#fff";
+    //this.toggled = !this.toggled;
+    //console.log("click")
   }
 
 }
