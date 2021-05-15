d6c069b9b313fe26dee2b94b2d24f17eb5c34987
Luis García Montañés <gamo.luis@gmail.com>
2021-05-15 11:34:12 +0200

    Data binding in input forms

--- art-project/src/app/app.component.ts
+++ art-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'art-project';
-  matrixHeightValue =10;
-  matrixWidthValue =10;
+  title = "Lewis' Art Project";
+  matrixHeightValue = 8;
+  matrixWidthValue = 8;
+  sliderValue = 8;
   sizeValue = 7;
-  items = [1,2,3,4,5,6,7,8,9];
+  items = [1,2,3,4,5,6,7,8];
+  
+  public constructor(private titleService: Title) {
+    this.titleService.setTitle(this.title);
+  }
+
+  public onWidthChanged(): void {
+    console.log(this.matrixWidthValue);
+    
+    this.items = [];
+
+    for(let i=0; i<this.matrixWidthValue; i++){
+      this.items.push(1);
+    }
+  }
+
+  public onHeightChanged(): void {
+    console.log(this.matrixHeightValue);
+  }
+
+  public onSliderChanged(): void {
+    console.log(this.sliderValue);
+  }
 }
